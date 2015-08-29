4c27aaa9acfff818a8b0994bc2cc6a1f5e0b10bb
e-9 <abraham@codefire.mx>
2015-08-29 15:59:47 -0500

    Cambio de front por completo
    Cambios al front, ya se sabe cuando se esta en el top para poder
    mostrar el nabab

--- aboutviry_ws/bio/static/bio/js/init.js
+++ aboutviry_ws/bio/static/bio/js/init.js
@@ -1,8 +1,23 @@
+var ABOUTV = ABOUTV || {};
+
+
 (function($){
   $(function(){
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+
+    var distance = $('#aboutv-sec1').offset().top,
+    $window = $(window);
+
+    $window.scroll(function() {
+        if ( $window.scrollTop() >= distance ) {
+            console.log('en la cima')
+        }
+        else if( $window.scrollTop() <= distance ) {
+            console.log('ya no en la cima')
+        }
+    });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
