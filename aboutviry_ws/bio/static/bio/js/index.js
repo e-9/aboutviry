681b445e3da12291f22c0ae292b1aaedd1d45ae8
e-9 <abraham@codefire.mx>
2015-09-02 09:46:24 -0500

    Nuevo front
    Nuevo front con imagen completa en background

---
+++ aboutviry_ws/bio/static/bio/js/index.js
@@ -0,0 +1,14 @@
+var ABOUTV = ABOUTV || {};
+
+
+$( document ).ready(function() {
+
+	$('#page-body').css({
+		'backgroundImage': 'url(/static/bio/images/background2.jpg)',
+		'backgroundRepeat': 'no-repeat',
+		'backgroundPosition': 'center center',
+    'backgroundSize': 'cover',
+    'backgroundAttachment': 'fixed'
+	});
+
+});
